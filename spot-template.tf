a2b80414c81112a5f7ca9150eb47ddd8dec0235b
rollrat <rollrat@naver.com>
2022-11-25 22:03:35 +0900

    Update spot-template.tf

--- spot-template.tf
+++ spot-template.tf
@@ -24,7 +24,7 @@ resource "aws_spot_instance_request" "%%instancename%%" {
 resource "time_sleep" "wait_30_seconds" {
   depends_on = [aws_spot_instance_request.%%instancename%%]
 
-  create_duration = "30s"
+  create_duration = "60s"
 }
 
 resource "aws_eip" "eip-%%instancename%%" {
