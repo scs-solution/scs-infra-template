6f2db0a02ab03d292a9158a5910650e20b1633a4
rollrat <rollrat@naver.com>
2022-11-27 16:26:17 +0900

    Update spot-template.tf

--- spot-template.tf
+++ spot-template.tf
@@ -1,12 +1,3 @@
-resource "aws_key_pair" "%%keyname%%" {
-  key_name   = "%%keyname%%"
-  public_key = file("%%keyname%%.pub")
-}
-
-data "aws_security_group" "default_%%instancename%%" {
-  name = "default"
-}
-
 resource "aws_spot_instance_request" "%%instancename%%" {
   ami           = "ami-0c76973fbe0ee100c" # amazon/amzn2-ami-kernel-5.10-hvm-2.0.20220912.1-x86_64-gp2
   instance_type = "%%instancespec%%"
